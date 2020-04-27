2644b3a0254fa23749f91f544f1bdc54aef2a714
ashish336b <ashish336b@gmail.com>
2020-04-27 23:14:55 +0545

    unmatch should be added if minimum two diff card is clicked and does not match

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var app = new Vue({
   data: {
     flipCard: false,
     canClick: true,
+    clickedIndex: [],
     clickedCard: {
       firstCard: "",
       secondCard: "",
@@ -12,17 +13,31 @@ var app = new Vue({
       flip: false,
       match: [],
     },
-    matchIndex: [],
     cards: [
+      {
+        id: 9,
+        class: "fa fa-diamond",
+        dataKind: "diamond",
+      },
       {
         id: 1,
         class: "fa fa-diamond",
         dataKind: "diamond",
       },
       {
+        id: 14,
+        class: "fa fa-paper-plane-o",
+        dataKind: "plane",
+      },
+      {
         id: 2,
         class: "fa fa-paper-plane-o",
         dataKind: "plane",
+      },
+      {
+        id: 6,
+        class: "fa fa-anchor",
+        dataKind: "anchor",
       },
       {
         id: 3,
@@ -35,54 +50,14 @@ var app = new Vue({
         dataKind: "bolt",
       },
       {
-        id: 5,
-        class: "fa fa-cube",
-        dataKind: "cube",
-      },
-      {
-        id: 6,
-        class: "fa fa-anchor",
-        dataKind: "anchor",
-      },
-      {
-        id: 7,
-        class: "fa fa-leaf",
-        dataKind: "leaf",
-      },
-      {
-        id: 8,
-        class: "fa fa-bicycle",
-        dataKind: "bicycle",
-      },
-      {
-        id: 9,
-        class: "fa fa-diamond",
-        dataKind: "diamond",
-      },
-      {
-        id: 10,
-        class: "fa fa-bomb",
-        dataKind: "bomb",
-      },
-      {
-        id: 11,
-        class: "fa fa-leaf",
-        dataKind: "leaf",
-      },
-      {
         id: 12,
         class: "fa fa-bolt",
         dataKind: "bolt",
       },
       {
-        id: 13,
-        class: "fa fa-bicycle",
-        dataKind: "bicycle",
-      },
-      {
-        id: 14,
-        class: "fa fa-paper-plane-o",
-        dataKind: "plane",
+        id: 5,
+        class: "fa fa-cube",
+        dataKind: "cube",
       },
       {
         id: 15,
@@ -90,25 +65,49 @@ var app = new Vue({
         dataKind: "cube",
       },
       {
+        id: 7,
+        class: "fa fa-leaf",
+        dataKind: "leaf",
+      },
+      {
+        id: 11,
+        class: "fa fa-leaf",
+        dataKind: "leaf",
+      },
+      {
+        id: 10,
+        class: "fa fa-bomb",
+        dataKind: "bomb",
+      },
+      {
         id: 16,
         class: "fa fa-bomb",
         dataKind: "bomb",
+      },
+      {
+        id: 13,
+        class: "fa fa-bicycle",
+        dataKind: "bicycle",
+      },
+      {
+        id: 8,
+        class: "fa fa-bicycle",
+        dataKind: "bicycle",
       },
     ],
   },
   methods: {
     matchCard: function () {
       this.canClick = true;
-      if (this.matchIndex.length === 2) {
-        console.log("matched");
-        this.matchIndex = [];
+      if (this.clickedIndex.length === 2) {
+        this.clickedIndex = [];
       }
-      this.matchIndex = [];
+      this.clickedIndex = [];
     },
     unflipCard: function () {
       setTimeout(
         function () {
-          this.matchIndex = [];
+          this.clickedIndex = [];
           this.styleClass.flip = false;
           this.canClick = true;
         }.bind(this),
@@ -124,15 +123,16 @@ var app = new Vue({
       if (!this.flipCard) {
         //first click
         this.flipCard = true;
-        this.matchIndex.push(card.id);
+        this.clickedIndex.push(card.id);
         this.clickedCard.firstCard = event.currentTarget;
       } else {
         //second click
         this.canClick = false;
         this.flipCard = false;
-        if (this.matchIndex.indexOf(card.id) === -1) {
-          this.matchIndex.push(card.id);
+        if (this.clickedIndex.indexOf(card.id) === -1) {
+          this.clickedIndex.push(card.id);
         } else {
+          this.clickedIndex = [];
           console.log("cannot push");
         }
 
@@ -143,9 +143,10 @@ var app = new Vue({
         ) {
           //if both clicks matches
           //check for unique matched index
-          if (this.styleClass.match.indexOf(...this.matchIndex) === -1) {
-            this.styleClass.match.push(...this.matchIndex);
+          if (this.styleClass.match.indexOf(...this.clickedIndex) === -1) {
+            this.styleClass.match.push(...this.clickedIndex);
           } else {
+            this.clickedIndex = [];
             console.log("cannot push");
           }
           this.matchCard();
